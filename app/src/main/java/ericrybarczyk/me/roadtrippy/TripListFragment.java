7bee6872d8d8da520b257e4d3e7da3000f6e90f3
Eric Rybarczyk <EricRybarczyk@gmail.com>
2018-09-24 19:47:16 -0400

    Fixed FAB click handler issue

--- app/src/main/java/ericrybarczyk/me/roadtrippy/TripListFragment.java
+++ app/src/main/java/ericrybarczyk/me/roadtrippy/TripListFragment.java
@@ -3,14 +3,23 @@ package ericrybarczyk.me.roadtrippy;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import ericrybarczyk.me.roadtrippy.util.FragmentTags;
+
 
 public class TripListFragment extends Fragment {
 
+    private FragmentNavigationRequestListener fragmentNavigationRequestListener;
+
+    @BindView(R.id.fab) protected FloatingActionButton fab;
 
     public TripListFragment() {
         // Required empty public constructor
@@ -30,25 +39,31 @@ public class TripListFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_trip_list, container, false);
+        ButterKnife.bind(this, rootView);
 
+        return rootView;
+    }
 
-        return inflater.inflate(R.layout.fragment_trip_list, container, false);
+    @OnClick(R.id.fab)
+    public void onClick(View view) {
+        fragmentNavigationRequestListener.onFragmentNavigationRequest(FragmentTags.TAG_CREATE_TRIP);
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-//        if (context instanceof OnFabDisplayRequestListener) {
-//            fabDisplayRequestListener = (OnFabDisplayRequestListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString() + " must implement OnFabDisplayRequestListener");
-//        }
+        if (context instanceof FragmentNavigationRequestListener) {
+            fragmentNavigationRequestListener = (FragmentNavigationRequestListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement FragmentNavigationRequestListener");
+        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-//        fabDisplayRequestListener = null;
+        fragmentNavigationRequestListener = null;
     }
 
 }
